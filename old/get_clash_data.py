59dc119d56900e3c4eb3702f2af4bf2cc11d5cc4
Noah Horsley <nphorsley59@gmail.com>
2022-10-30 15:39:48 -0400

    Refactor clan data request functions

--- old/get_clash_data.py
+++ old/get_clash_data.py
@@ -1,49 +1,82 @@
-import datetime as dt
-
 import json
 import pandas as pd
 import requests
 
 from config import Config
-from old.utils import export
+from old import utilities 
 
 
-today_date = dt.datetime.today().strftime("%m-%d-%Y %H:%M:%S")
-authorization = {
+AUTHORIZATION = {
     'Accept': 'application/json',
     'authorization': f'Bearer {Config.CLASH_API_KEY}'
 }
 
 
-def get_clan_data(clantag):
-    url = 'https://api.clashofclans.com/v1/clans/%23{}/members'.format(clantag)
-    response = requests.get(url, headers=authorization).text
+def request_clan_data(clan_tag: str) -> pd.DataFrame:
+    """
+    Request clan data from Clash of Clans API.
+    Args:
+        clan_tag (str): Clan tag.
+    """
+    url = 'https://api.clashofclans.com/v1/clans/%23{}/members'.format(clan_tag)
+    response = requests.get(url, headers=AUTHORIZATION).text
     js = json.loads(response)['items']
     df = pd.DataFrame(js)
     return df
 
 
-def get_player_data(playertag):
-    url = 'https://api.clashofclans.com/v1/players/%23{}'.format(playertag)
+def extract_player_tags(clan_data: pd.DataFrame) -> list[str]:
+    """
+    Extract player tags from clan data.
+    Args:
+        clan_data (pd.DataFrame): Clan data.
+    """
+    player_tags = clan_data['tag'].apply(lambda x: x.replace('#', '', 1))
+    return player_tags.tolist()
+
+
+def request_player_data(player_tag: str) -> pd.DataFrame:
+    """
+    Request player data from Clash of Clans API.
+    Args:
+        player_tag (str): Player tag.
+    """
+    url = 'https://api.clashofclans.com/v1/players/%23{}'.format(player_tag)
     response = requests.get(url, headers=authorization).text
     js = json.loads(response)
     df = pd.DataFrame.from_dict(js, orient='index')
     return df
 
 
-def get_clash_data(clantag):
-    clan_data = get_clan_data(clantag=clantag)
-    player_data = pd.DataFrame()
-    tags = clan_data['tag'].apply(lambda x: x.replace('#', '', 1)).tolist()
-    for tag in tags:
-        player_data = pd.concat([player_data, get_player_data(playertag=tag).T])
-    export(clan_data, "data/clan_data/clan_data.csv", timestamped_copy=True)
-    export(player_data, "data/player_data/player_data.csv", timestamped_copy=True)
-    return clan_data, player_data
+def request_clan_player_data(clan_tag: pd.DataFrame) -> pd.DataFrame:
+    """
+    Get player data for all players in clan.
+    Args:
+        clan_tag (pd.DataFrame): Clan tag.
+    """
+    clan_data = request_clan_data(clan_tag)
+    player_data = []
+    player_tags = extract_player_tags(clan_data)
+    for tag in player_tags:
+        players.append(request_player_data(tag))
+    return player_data
+
+
+def main(clan_tag: str) -> pd.DataFrame:
+    """
+    Request and export player data for all players in clan.
+    Args:
+        clan_tag (str): Clan tag.
+    """
+    player_data = request_clan_player_data(clan_tag)
+    utilities.export(player_data, 
+                     "data/player_data/player_data.csv",
+                     timestamped_copy=True)
+    return player_data
 
 
 if __name__ == '__main__':
     test_clantag = 'YQLJ9CRU'
-    test_clan_data, test_player_data = get_clash_data(clantag=test_clantag)
-    print(test_clan_data.sample(5))
+    test_player_data = main(test_clantag)
     print(test_player_data.sample(5))
+
